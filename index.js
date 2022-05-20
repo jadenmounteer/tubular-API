56ef00789b292386200f9911c937ff9201fb491a
Jaden Mounteer <67714386+jadenmounteer@users.noreply.github.com>
2022-05-20 07:47:09 -0600

    install dotenv

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const app = express();
 const mongodb = require('./db/connect');
-
-app.use(bodyParser.json());
-
 const port = process.env.PORT || 3000;
+const app = express();
 
-app.use('/', require('./routes'));
+app
+  .use(bodyParser.json())
+  .use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    next();
+  })
+  .use('/', require('./routes'));
 
 mongodb.initDb((err) => {
   if (err) {
