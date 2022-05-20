692ae8127208864495349559a530b2123d802654
Jaden Mounteer <67714386+jadenmounteer@users.noreply.github.com>
2022-05-19 21:16:37 -0600

    Update index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.use(bodyParser.json());
+
+const port = 3000;
+
+app.use('/', require('./routes'));
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
+});
