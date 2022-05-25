c1033d3fe665a993450be13a48d5d8b3b0c9f747
Jaden Mounteer <67714386+jadenmounteer@users.noreply.github.com>
2022-05-25 09:10:08 -0600

    Enable CORS

--- index.js
+++ index.js
@@ -1,21 +1,25 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongodb = require('./db/connect');
+const cors = require('cors');
 
 const port = process.env.PORT || 3000;
 const app = express();
 
+app.use(cors());
+// This makes it accessible to anyone.
+// To configure options, use this tutorial: https://stackabuse.com/handling-cors-with-node-js/
+
 app
   .use(bodyParser.json())
-  .use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader(
-      'Access-Control-Allow-Headers',
-      'Origin, X-Requested-With, Content-Type, Accept'
-    );
-
-    next();
-  })
+  // .use((req, res, next) => {
+  //   res.header('Access-Control-Allow-Origin', '*');
+  //   res.header(
+  //     'Access-Control-Allow-Headers',
+  //     'Origin, X-Requested-With, Content-Type, Accept'
+  //   );
+  //   next();
+  // })
   .use('/', require('./routes'));
 
 mongodb.initDb((err) => {
